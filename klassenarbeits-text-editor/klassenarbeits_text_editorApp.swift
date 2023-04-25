99781cd29781b9b364cf9e8516bfac969c8c7953
David <david@davidcraft.de>
2023-04-25 16:34:47 +0200

    Initial Commit

---
+++ klassenarbeits-text-editor/klassenarbeits_text_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  klassenarbeits_text_editorApp.swift
+//  klassenarbeits-text-editor
+//
+//  Created by David Polster on 25.04.23.
+//
+
+import SwiftUI
+
+@main
+struct klassenarbeits_text_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
