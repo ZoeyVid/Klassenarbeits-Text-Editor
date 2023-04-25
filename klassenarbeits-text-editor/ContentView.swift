b63d147840d74b3719bbe302151134d8f7118da8
David <david@davidcraft.de>
2023-04-25 16:37:27 +0200

    Initial Commit

--- klassenarbeits-text-editor/ContentView.swift
+++ klassenarbeits-text-editor/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    let defaults = UserDefaults.standard
+    @State private var isPresentingConfirm: Bool = false
+    @State private var editor = 0
+    @State var textMain: String = "Viel Glück!"
+    @State var textPlan: String = "Glück Viel!"
+    @State var textNote: String = "Glückliche Viele!"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+          Picker("What is your favorite color?", selection: $editor) {
+              Text("Text").tag(0)
+              Text("Schreibplan").tag(1)
+              Text("Notizen").tag(2)
+          } .pickerStyle(.segmented)
+                .frame(width: 300, height: 30, alignment: .centerLastTextBaseline)
+            if editor == 0 {
+                TextEditor(text: $textMain)
+                    .padding()
+                      .keyboardType(.alphabet)
+                      .disableAutocorrection(true)
+            } else if editor == 1 {
+                TextEditor(text: $textPlan)
+                    .padding()
+                      .keyboardType(.alphabet)
+                      .disableAutocorrection(true)
+            } else if editor == 2 {
+                TextEditor(text: $textNote)
+                    .padding()
+                      .keyboardType(.alphabet)
+                      .disableAutocorrection(true)
+            }
+                Spacer()
+                .navigationTitle("Klassenarbeit")
+                        .toolbar {
+                            ShareLink(item: textMain)
+
+                            Button("Laden") {
+                                textMain = defaults.object(forKey: "textMain") as! String
+                                textPlan = defaults.object(forKey: "textPlan") as! String
+                                textNote = defaults.object(forKey: "textNote") as! String
+                            }
+                            
+                            Button("Speichern") {
+                                defaults.set(textMain, forKey: "textMain")
+                                defaults.set(textPlan, forKey: "textPlan")
+                                defaults.set(textNote, forKey: "textNote")
+                            }
+                            
+                            Button("Alles Löschen") {
+                                isPresentingConfirm = true
+                            }
+                            .tint(.red)
+                            .confirmationDialog("Sicher?",
+                              isPresented: $isPresentingConfirm) {
+                              Button("Ja alles löschen", role: .destructive) {
+                                  textMain = "Viel Glück!"
+                                  defaults.set("Viel Glück!", forKey: "textMain")
+                                  textPlan = "Glück Viel!"
+                                  defaults.set("Glück Viel!", forKey: "textPlan")
+                                  textNote = "Glückliche Viele!"
+                                  defaults.set("Glückliche Viele!", forKey: "textNote")
+                                  isPresentingConfirm = false
+                               }
+                             }
+                        }
+                }
     }
 }
 
